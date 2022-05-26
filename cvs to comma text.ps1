a7c70b22ff3c6d0e8a7162de24426612fb4b0729
Sean-Garnett-II <68874649+Sean-Garnett-II@users.noreply.github.com>
2022-05-26 13:16:35 -0400

    Update cvs to comma text.ps1
    Added input for any type of delimiter

--- cvs to comma text.ps1
+++ cvs to comma text.ps1
@@ -1,15 +1,20 @@
-
 $fileName = Read-Host "File Name: "
+$delimiter = Read-Host "Delimiter: "
+if($delimiter -eq ""){
+  $delimniter = ", "
+  }
 if(Test-Path -Path $fileName){
-New-Item -path .\ -type file -name "$fileName.txt"
-} else {
-echo "Bad file Name"
-pause
+  if(Test-Path -Path "$fileName.txt") {
+  Remove-Item -Path "$fileName.txt" -Force
+  }
+  New-Item -path .\ -type file -name "$fileName.txt"
+  Get-Content -Path $fileName | ForEach-Object {
+    $value = "$_"+"$delimiter"
+    Add-Content -Path .\"$fileName.txt" -Value $value -NoNewline
+    }
+  } else {
+  echo "Bad file Name"
+  pause
 }
-
-Get-Content -Path $fileName | ForEach-Object {
-Add-Content -Path .\"$fileName.txt" -Value "$_, " -NoNewline
-}
-
 
 pause
