73391759da7ea9cb7e332a5dd20e35e0d656e12e
Sean-Garnett-II <68874649+Sean-Garnett-II@users.noreply.github.com>
2022-06-09 13:35:16 -0400

    Expanded clipboard cmdlet

--- Clipboard cmdlet.ps1
+++ Clipboard cmdlet.ps1
@@ -1,9 +1,14 @@
 $fileName = Read-Host "File Name: "
 if(Test-Path -Path $fileName){
+$length = (Get-Content -Path $fileName ).Length
+$count = 1
 Get-Content -Path $fileName | ForEach-Object {
 Set-Clipboard -Value "$_"
 echo "$_"
+echo "$count / $length"
+$count+=1
 pause
+cls
 }
 } else {
 echo "Bad file Name"
